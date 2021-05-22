7708660c121e9d912feaddefb7752f9174e129f5
DmitryEfremov <dmitry.efremov@quantumsoft.ru>
2021-05-23 01:22:48 +0700

    sub-2 Added 2 folder: subtitles_org, subtitles_reg to .gitignore.

---
+++ sub-2.py
@@ -0,0 +1,14 @@
+from sys import argv
+# from parser.series import ParseWordsSeries
+
+
+name_sub_series = argv[1]
+percent = argv[2]
+
+print(argv[1], argv[2])
+
+# pars = ParseWordsSeries(name_folder=name_folder)
+# pars.scan_all_files(num_series)
+# print(len(pars.set_words))
+
+# pars.write_file_csv()
